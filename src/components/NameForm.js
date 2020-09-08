d464b80038aea1b9ef0199502274ccf941c266c7
Heng-Hao Pu <henghaopu@Heng-Haos-MacBook-Pro.local>
2020-09-07 20:29:51 -0500

    Abstract out handleChange and handleSubmit from NameForm to useForm

--- src/components/NameForm.js
+++ src/components/NameForm.js
@@ -1,56 +1,46 @@
-import React from 'react';
+import React from 'react'
+import useForm from '../hooks/useForm'
 
 class NameForm extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = { name: '', email: '' };
+    super(props)
+    this.state = { name: '', email: '' }
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ [event.target.id]: event.target.value });
-  }
-
-  handleSubmit(event) {
-    alert(
-      `A name and email set was submitted: ${this.state.name} ${this.state.email}`
-    );
-    event.preventDefault();
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <div style={{ margin: '1rem' }}>
-          <label style={{ marginRight: '1rem' }} htmlFor='name'>
+          <label style={{ marginRight: '1rem' }} htmlFor="name">
             Name
           </label>
           <input
-            id='name'
-            type='text'
+            id="name"
+            type="text"
             value={this.state.value}
             onChange={this.handleChange}
           />
         </div>
         <div style={{ margin: '1rem' }}>
-          <label style={{ marginRight: '1rem' }} htmlFor='email'>
+          <label style={{ marginRight: '1rem' }} htmlFor="email">
             Email
           </label>
           <input
-            id='email'
-            type='text'
+            id="email"
+            type="text"
             value={this.state.value}
             onChange={this.handleChange}
           />
         </div>
         <div style={{ margin: '1rem' }}>
-          <input type='submit' value='Submit' />
+          <input type="submit" value="Submit" />
         </div>
       </form>
-    );
+    )
   }
 }
 
-export default NameForm;
+export default NameForm
---
+++ src/hooks/useForm.js
@@ -0,0 +1,14 @@
+function useForm(props) {
+  // handleChange(event) {
+  //   this.setState({ [event.target.id]: event.target.value })
+  // }
+
+  // handleSubmit(event) {
+  //   alert(
+  //     `A name and email set was submitted: ${this.state.name} ${this.state.email}`
+  //   )
+  //   event.preventDefault()
+  // }
+
+  return {}
+}
