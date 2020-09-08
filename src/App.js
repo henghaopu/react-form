96b522e7173436a77f3b75b9e3da3fa824affedb
Heng-Hao Pu <henghaopu@Heng-Haos-MacBook-Pro.local>
2020-09-07 19:50:16 -0500

    Grab an example form from React docs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import NameForm from './components/NameForm';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <NameForm />
+    </div>
+  );
 }
 
 export default App;
