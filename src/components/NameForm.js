73642ee5c8e82ba47e226e70a562e3b642350eaa
Heng-Hao Pu <henghaopu@Heng-Haos-MacBook-Pro.local>
2020-09-07 20:38:51 -0500

    Get back handleChange and handleSubmit from useForm to NameForm

--- src/components/NameForm.js
+++ src/components/NameForm.js
@@ -1,46 +1,39 @@
 import React from 'react'
 import useForm from '../hooks/useForm'
 
-class NameForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { name: '', email: '' }
+function NameForm() {
+  // get these back from useForm
+  const { handleSubmit, handleChange, ...state } = useForm()
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div style={{ margin: '1rem' }}>
-          <label style={{ marginRight: '1rem' }} htmlFor="name">
-            Name
-          </label>
-          <input
-            id="name"
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div style={{ margin: '1rem' }}>
-          <label style={{ marginRight: '1rem' }} htmlFor="email">
-            Email
-          </label>
-          <input
-            id="email"
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div style={{ margin: '1rem' }}>
-          <input type="submit" value="Submit" />
-        </div>
-      </form>
-    )
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div style={{ margin: '1rem' }}>
+        <label style={{ marginRight: '1rem' }} htmlFor="name">
+          Name
+        </label>
+        <input
+          id="name"
+          type="text"
+          value={state.value}
+          onChange={handleChange}
+        />
+      </div>
+      <div style={{ margin: '1rem' }}>
+        <label style={{ marginRight: '1rem' }} htmlFor="email">
+          Email
+        </label>
+        <input
+          id="email"
+          type="text"
+          value={state.value}
+          onChange={handleChange}
+        />
+      </div>
+      <div style={{ margin: '1rem' }}>
+        <input type="submit" value="Submit" />
+      </div>
+    </form>
+  )
 }
 
 export default NameForm
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -12,3 +12,5 @@ function useForm(props) {
 
   return {}
 }
+
+export default useForm
