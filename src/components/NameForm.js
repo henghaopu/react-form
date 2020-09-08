05d67b252d8607e1ca6de5c2cb6c1062f932766b
Heng-Hao Pu <henghaopu@Heng-Haos-MacBook-Pro.local>
2020-09-07 20:09:58 -0500

    Add an email field

--- src/components/NameForm.js
+++ src/components/NameForm.js
@@ -3,33 +3,51 @@ import React from 'react';
 class NameForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: '' };
+    this.state = { name: '', email: '' };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ value: event.target.value });
+    this.setState({ [event.target.id]: event.target.value });
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+    alert(
+      `A name and email set was submitted: ${this.state.name} ${this.state.email}`
+    );
     event.preventDefault();
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
+        <div style={{ margin: '1rem' }}>
+          <label style={{ marginRight: '1rem' }} htmlFor='name'>
+            Name
+          </label>
           <input
+            id='name'
             type='text'
             value={this.state.value}
             onChange={this.handleChange}
           />
-        </label>
-        <input type='submit' value='Submit' />
+        </div>
+        <div style={{ margin: '1rem' }}>
+          <label style={{ marginRight: '1rem' }} htmlFor='email'>
+            Email
+          </label>
+          <input
+            id='email'
+            type='text'
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div style={{ margin: '1rem' }}>
+          <input type='submit' value='Submit' />
+        </div>
       </form>
     );
   }
